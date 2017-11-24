7d3da42983ac44462c60c1247502ad367d739519
Vamshi Krishna Gujjari <vamshi@Vamshis-MacBook-Air.local>
2017-11-24 23:37:44 +0530

    first commit

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb://vamshi:1111@ds033956.mlab.com:33956/tasky-prod'}
+  module.exports = {mongoURI: 'mongodb://<UserName>:<Password>@ds033956.mlab.com:33956/tasky-prod'}
 } else {
   module.exports = {mongoURI: 'mongodb://localhost/tasky-dev'}
 }
